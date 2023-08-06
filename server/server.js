b8d39e481234a9700b053c25f1fdae9d3340addd
luisvillarreal <secretangel21@outlook.com>
2023-08-06 17:29:43 -0600

    fix again

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '../client/dist/index.html')));
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
